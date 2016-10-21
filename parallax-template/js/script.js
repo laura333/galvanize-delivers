13063a35d4546f8d4be18aa0e020608d5125a9b3
Laura Baukol <laura.baukol@mac.com>
2016-10-21 16:53:54 -0600

    click to add items and prices

--- parallax-template/js/script.js
+++ parallax-template/js/script.js
@@ -1,4 +1,32 @@
 "use strict";
 console.log("hi");
 
-var
+// var orderBurger = document.querySelector("body > div.container > div > div.col.s4.l6 > div > div:nth-child(1) > div > div.card-action > a");
+var cards = document.querySelectorAll("div.card");
+// console.log(orderBurger);
+
+var tempCard = {};
+
+for(var i = 0; i < cards.length; i++) {
+  tempCard = cards[i];
+  tempCard.addEventListener('click', function(evt) {
+    var itemDetails = {};
+    var itemContent = this.querySelector('div.card-content');
+    var itemName = itemContent.querySelectorAll('p')[0];
+    var itemPrice = itemContent.querySelectorAll('p')[1];
+    itemDetails['name'] = itemName.innerText;
+    itemDetails['price'] = itemPrice.innerText;
+    console.log(itemDetails);
+    buildTable(itemDetails);
+  });
+}
+function buildTable(itemDetails) {
+  var table = document.getElementById('table');
+  var tr = document.createElement('tr');
+  var tdName = tr.appendChild(document.createElement('td'));
+  var tdPrice = tr.appendChild(document.createElement('td'));
+  tdPrice.setAttribute("class", "right-align");
+  tdName.innerHTML = itemDetails.name;
+  tdPrice.innerHTML = itemDetails.price;
+    table.appendChild(tr);
+}
